c6c6e109c1f132b555b9b363372d45209da75756
leninqwerty03@gmail.com <Deltaforge@2025>
2026-04-14 19:01:28 +0530

    fix: Correct locator for password field in test_has_title function

--- tests/test.py
+++ tests/test.py
@@ -7,7 +7,7 @@ def test_has_title(page: Page):
     # Expect a title "to contain" a substring.
     expect(page).to_have_title("Test Login | Practice Test Automation")
     page.fill("#username", "student")
-    page.fill("#password", "Password123")
+    page.fill("#cred", "Password123")
     page.click("#submit")
 
     # Expect the logged-in page title.
